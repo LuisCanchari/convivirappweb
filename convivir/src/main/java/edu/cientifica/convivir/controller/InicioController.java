3d370f6c469fbedd5b1a2d9ea8cb05327ff3c173
Luis Canchari <luis.andres.canchari@gmail.com>
2021-02-15 21:24:05 -0500

    pagina_principal

--- convivir/src/main/java/edu/cientifica/convivir/controller/InicioController.java
+++ convivir/src/main/java/edu/cientifica/convivir/controller/InicioController.java
@@ -12,32 +12,45 @@ import edu.cientifica.convivir.service.UsuarioService;
 @Controller
 @RequestMapping("/")
 public class InicioController {
-	
+
 	@Autowired
 	private UsuarioService usuarioService;
-	
-	@GetMapping({"/index","/home", "/"})
+
+	@GetMapping({ "/index", "/" })
 	public String inicio() {
-		return "inicio";
+		return "index";
 	}
-	
+
 	@GetMapping("/login")
 	public String loginForm() {
-		
-		return "login";
+
+		return "inicio_login";
 	}
-	
+
 	@RequestMapping("/validar")
-	public String validarUsuario(
-			@RequestParam (name = "username") String user, 
-			@RequestParam(name ="password") String pass){
-		
-		if 	(usuarioService.validarUsuario(user, pass)) {
-			return "inicio";
-			
-		}else {
-			return "login";
-			
+	public String validarUsuario(@RequestParam(name = "username") String user,
+			@RequestParam(name = "password") String pass) {
+
+		if (usuarioService.validarUsuario(user, pass)) {
+			return "inicio_principal";
+
+		} else {
+			return "inicio_login";
+
 		}
+	}
+
+	@RequestMapping("/salir")
+	public String salir() {
+
+		return "index";
+
+	}
+
+	@RequestMapping("/registrar")
+	public String registrar() {
+
+		return "inicio_registrar";
+
 	}
 }
